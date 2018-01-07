56d355d9e1261076aeda2be529b2e0048a0f4013
lucek <lukasz.odziewa@gmail.com>
2018-01-07 22:22:22 +0100

    created contract handling routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # User signup & authentication
   post 'signup',     to: 'users#create'
   post 'auth/login', to: 'authentication#authenticate'
+
+  # Contracts
+  resources :contracts, only: [:index, :create, :show, :destroy]
 end
