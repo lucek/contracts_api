b46359928ebd98539964e4afd87edf3abbba71d8
lucek <lukasz.odziewa@gmail.com>
2018-01-07 23:54:10 +0100

    created user and contract factories

---
+++ spec/factories/factories.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :contract do
+    vendor    { Faker::Lorem.word }
+    starts_on { Date.today }
+    ends_on   { Faker::Date.forward(365) }
+    price     { Faker::Number.number(100) }
+    user      { create :user }
+  end
+
+  factory :user do
+    full_name { Faker::Zelda.character }
+    email     'foo@bar.com'
+    password  'v0lders'
+  end
+end
