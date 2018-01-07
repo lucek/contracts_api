a9a94135d30a7d61995fbc1465ddfc5ae50baa63
lucek <lukasz.odziewa@gmail.com>
2018-01-08 00:38:41 +0100

    added validation of email's uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many              :contracts, dependent: :destroy
-  validates_presence_of :full_name, :email, :password_digest
+  has_many                :contracts, dependent: :destroy
+  validates_presence_of   :full_name, :email, :password_digest
+  validates_uniqueness_of :email
 end
--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe User, type: :model do
   it { should have_many(:contracts).dependent(:destroy) }
   it { should validate_presence_of(:full_name) }
   it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
   it { should validate_presence_of(:password) }
 end
