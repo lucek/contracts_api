336566eab0f915370a43710e58a477ed859ab24a
lucek <lukasz.odziewa@gmail.com>
2018-01-07 22:54:03 +0100

    added dependent: destroy to User Contract relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many              :contracts
+  has_many              :contracts, dependent: :destroy
   validates_presence_of :full_name, :email, :password_digest
 end
